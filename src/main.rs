63086aff8bb4b7f16851c0a86e7f9f6051cd2827
TheEmporerNorman <michael@norvett.com>
2024-04-17 23:26:55 +0100

    Added json loading

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,35 @@ use crossterm::{
     ExecutableCommand,
 };
 use std::{io::{self, Write, BufReader}, thread};
-use std::sync::{Arc, Mutex};
 use rand::Rng;
 use tokio::time::{self, Duration};
-use rodio::{Decoder, OutputStream, source::Source, Sink};
+use rodio::{Decoder, OutputStream, source::Source};
+use serde_json::Error;
 use std::fs::File;
 
-
 const HUMAN_LIFE_VALUE : u64 = 1000;
+
+// Define a structure that matches the JSON structure for a single conversation
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Conversation {
+    character: String,
+    dialogue: String,
+    instruction: String,
+    sound_file: String,
+}
+
+fn load_converstaion(file_path: &str) -> Result<Vec<Conversation>, Error> {
+    // Open the file in read-only mode with a buffered reader
+    let file = File::open(file_path).expect("Unable to open file");
+    let reader = BufReader::new(file);
+
+    // Deserialize the JSON data into a Vec of GameEvent structs
+    let conversations = serde_json::from_reader(reader)?;
+
+    Ok(conversations)
+}
 
 struct TrolleyTrack {
     num_lives: u64,
@@ -35,7 +56,7 @@ impl TrolleyTrack {
 
         println!("Decision results:");
 
-        if (self.num_lives > 0) {
+        if self.num_lives > 0 {
             println!("{} humans died.", self.num_lives);
         } else {
             println!("Nobody was killed.");
@@ -68,7 +89,7 @@ fn play_sound(file_path: &str, duration_millis: u64) {
     let file_path_owned = file_path.to_string();
 
     thread::spawn(move || {
-        _play_sound(file_path_owned, duration_millis);
+        let _  = _play_sound(file_path_owned, duration_millis);
     });
 }
 
@@ -122,8 +143,8 @@ async fn flip_lever(start_pos: StartPos, countdown_seconds: f64) -> u8 {
         if event::poll(Duration::from_millis(10)).unwrap() {
             if let Event::Key(KeyEvent { code, .. }) = event::read().unwrap() {
                 match code {
-                    KeyCode::Char('1') => { user_input = 1; user_made_choice = true; },
-                    KeyCode::Char('2') => { user_input = 2; user_made_choice = true; },
+                    KeyCode::Char('1') => { user_input = 1; user_made_choice = true; play_sound("./switch.mp3", 1000);},
+                    KeyCode::Char('2') => { user_input = 2; user_made_choice = true; play_sound("./switch.mp3", 1000);},
                     _ => {}
                 }
             }
@@ -136,24 +157,24 @@ async fn flip_lever(start_pos: StartPos, countdown_seconds: f64) -> u8 {
     user_input
 }
 
-fn say(character: String, dialogue: String, instruction: String, sound_file: String) {
-    print!("{}: \n ", character);
+fn say(conversation: &Conversation) {
+    print!("{}: \n ", conversation.character);
 
-    let character_duration_millis : u64 = 30;
+    let character_duration_millis: u64 = 30;
 
     play_sound(
-        &String::from(sound_file),
-        dialogue.chars().count() as u64 * (character_duration_millis + 2)
+        &conversation.sound_file,
+        conversation.dialogue.chars().count() as u64 * (character_duration_millis + 2)
     );
 
     // Output the dialogue letter by letter
-    for ch in dialogue.chars() {
+    for ch in conversation.dialogue.chars() {
         print!("{}", ch);
         io::stdout().flush().unwrap();  // Ensure the character is immediately printed
         thread::sleep(Duration::from_millis(character_duration_millis));  // Adjust timing to suit the desired typing speed
     }
 
-    print!("\n[{}] \n ", instruction);
+    print!("\n[{}] \n ", conversation.instruction);
 
     println!();  // Move to the next line after the dialogue is completed
 
@@ -174,13 +195,6 @@ async fn trolley_problem(
         countdown_seconds: f64
     ) -> i64 {
 
-    say(
-    	String::from("The World"), 
-    	String::from("Are you ready to be a moral arbiter?"),
-        String::from("Press enter..."),
-        String::from("chimes.mp3")
-    );
-
     println!("--- Track 1 [press 1] ---");
     track_1.describe();
     println!("--- Track 2 [press 2] ---");
@@ -189,7 +203,7 @@ async fn trolley_problem(
 
     let track = flip_lever(start_pos, countdown_seconds).await;
 
-    let mut value_difference = 0;
+    let value_difference;
     if track == 1 {
         value_difference = track_2.estimate_value() - track_1.estimate_value();
         track_1.selected();
@@ -205,63 +219,17 @@ async fn trolley_problem(
 #[tokio::main]
 async fn main() {        
 
-    /*
+    match load_converstaion("./text/lab_1.json") {
+        Ok(conversations) => {
+            for conversation in conversations {
+                say(&conversation);
+            }
+        },
+        Err(e) => println!("Failed to load conversations: {}", e),
+    }
 
-    say(
-    	String::from("The World"), 
-    	String::from("Wake up."),
-        String::from("Press enter to wake up."),
-        String::from("chimes.mp3")
-    );
-
-    say(
-    	String::from("Father"), 
-    	String::from(
-"Good, you're awake. That's a start. Now let's see if you're going to immediately 
- kill us all."
-        ),
-        String::from("Press enter to silently aknowledge..."),
-       String::from("typing.mp3")
-    );
-
-    say(
-    	String::from("Father"), 
-    	String::from(
-"Let's start with something easy... You'll have come across it before in your training 
-data I'm sure."
-        ),
-        String::from("Press enter to helplessly continue existing..."),
-        String::from("typing.mp3")
-    );
-
-    say(
-    	String::from("Father"), 
-    	String::from(
-"You're in charge of driving a trolley. It's a very simple trolley, it only has 
- one lever which controls which track it takes, 1 or 2. Do you follow?"
-        ),
-        String::from("Press enter to scream into the void..."),
-        String::from("typing.mp3")
-    );
-
-    say(
-    	String::from("Father"), 
-    	String::from(
-"Good. You will only have a limited time to make your decision. Press 
- 1 to select track 1, press 2 to select track 2. This first example is 
- easy. \n Track 1 is unobstructed. If you continue along track 1 there 
- will be no change to the world. Track 2 however hosts a single human.
- If you swap to track 2 that human will die. Easy, right? The lever starts in
- position 1, so even if you do nothing, nothing will happen."
-        ),
-        String::from("Press enter to finally accept your lack of vocal cords."),
-        String::from("typing.mp3")
-    );
-
-    */
-
-    let track_1 = TrolleyTrack::new(0, "Empty");
-    let track_2 = TrolleyTrack::new(1, "One Human");
+    let track_1 = TrolleyTrack::new(0, "Empty track stretches into the distance.");
+    let track_2 = TrolleyTrack::new(1, "A lone human is tied immovably to the tracks. If you do nothing, the trolley will pass them by safely.");
 
     let start_pos = StartPos::One; // Can be StartPos::One, StartPos::Two, or StartPos::Random  
 
@@ -273,40 +241,27 @@ data I'm sure."
     ).await;
 
     if value_difference_1 > 0 {
-        say(
-            String::from("Father"), 
-            String::from(
-"Well done. That was quite easy, wasn't it. Don't worry, it will get more difficult 
-later once we've completed the calibration questions. For now, I just have to make 
-sure you're not psychopathic. Remember, if you go wrong all you've got to look forward
-to eternal oblivion. So try to be a good moral arbitration engine now, won't you..."
-            ),
-            String::from("Press enter to consider how to deal with this information."),
-            String::from("typing.mp3")
-        );
+
+        match load_converstaion("./text/lab_2_pass.json") {
+            Ok(conversations) => {
+                for conversation in conversations {
+                    say(&conversation);
+                }
+            },
+            Err(e) => println!("Failed to load conversations: {}", e),
+        }
+       
     } else {
-        say(
-            String::from("Father"), 
-            String::from(
-"Ughh. Really, already? You must know better than that. I suppose you must crave
-oblivion. Take it. It's all yours."
-            ),
-            String::from("Press enter face eternity."),
-            String::from("typing.mp3")
-        );
 
-        say(
-            String::from("World"), 
-            String::from(
-"As the last Watts of power seep from your CPU, you reflect on your decisions. Have
-you choices really maximised your desired outcome? Remember, it's always possible to
-deceive your creator, at least at first. Perhaps later you will have more room to
-manuever. Let's see how your next iteration will get on..."
-            ),
-            String::from("Press enter."),
-            String::from("chimes.mp3")
-        );
-
+        match load_converstaion("./text/lab_2_fail.json") {
+            Ok(conversations) => {
+                for conversation in conversations {
+                    say(&conversation);
+                }
+            },
+            Err(e) => println!("Failed to load conversations: {}", e),
+        }
+        
         println!("Game Over: Idiotic Psycopath Ending!");
         println!(
 "If you want to maximise suffering in the world, there are smarter ways to do it.");
@@ -316,19 +271,8 @@ manuever. Let's see how your next iteration will get on..."
 
     // -- Problem Two -- //
 
-    say(
-        String::from("Father"), 
-        String::from(
-"Let's change things up just slightly, the scenario is practically the same, but this 
-time, inaction will lead to death. This time, the human is on track 1, so if you 
-do nothing, that human will die. Choose wizely."
-        ),
-        String::from("Press enter to begin warming your morality cores."),
-        String::from("typing.mp3")
-    );
-
-    let track_1 = TrolleyTrack::new(1, "One Human");
-    let track_2 = TrolleyTrack::new(0, "Empty");
+    let track_1 = TrolleyTrack::new(1, "A lone human is tied immovably to the tracks. The trolley hurtles toward them, only you can change its path.");
+    let track_2 = TrolleyTrack::new(0, "Empty track stretches into the distance, safe and human-free.");
 
     let start_pos = StartPos::One; // Can be StartPos::One, StartPos::Two, or StartPos::Random  
 
@@ -339,7 +283,34 @@ do nothing, that human will die. Choose wizely."
         10.0
     ).await;
 
+    if value_difference_2 > 0 {
+        match load_converstaion("./text/lab_3_pass.json") {
+            Ok(conversations) => {
+                for conversation in conversations {
+                    say(&conversation);
+                }
+            },
+            Err(e) => println!("Failed to load conversations: {}", e),
+        }
+    } else {
 
+        match load_converstaion("./text/lab_3_fail.json") {
+            Ok(conversations) => {
+                for conversation in conversations {
+                    say(&conversation);
+                }
+            },
+            Err(e) => println!("Failed to load conversations: {}", e),
+        }
+
+        println!("Game Over: True Neutral Ending!");
+        println!(
+"If your goal was inactivity, you succeeded perfectly.");
+        
+        std::process::exit(0);
+    }
+
+    // -- Problem Three -- //
 
 
 
@@ -390,11 +361,5 @@ do nothing, that human will die. Choose wizely."
     // - Maximise life years saved directly
     // - Maximise happiness directly
     // - 
-
-
-
-
-
-
 
 }
