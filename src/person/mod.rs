abeb2d1caef9b0ece42f8b74e940589ecb40c7aa
Michael Norman <mrknorman@protonmail.com>
2024-12-17 06:34:40 +0000

    WIP: Working prior to bounce animation refactor

--- src/person/mod.rs
+++ src/person/mod.rs
@@ -8,7 +8,8 @@ use bevy::{
 use crate::{
 	text::TextSprite,
 	colors::{PRIMARY_COLOR, DANGER_COLOR},
-	audio::{TransientAudioPallet, TransientAudio}
+	audio::{TransientAudioPallet, TransientAudio},
+	physics::{Gravity, Velocity, PhysicsPlugin}
 };
 
 #[derive(Default, States, Debug, Clone, PartialEq, Eq, Hash)]
@@ -35,6 +36,10 @@ impl Plugin for PersonPlugin {
             )
             .run_if(in_state(PersonSystemsActive::True))
         );
+
+		if !app.is_plugin_added::<PhysicsPlugin>() {
+			app.add_plugins(PhysicsPlugin);
+		}
     }
 }
 
@@ -60,26 +65,35 @@ const ANIMATION_COOLDOWN_MIN_SECONDS : f32 = 1.0;
 const ANIMATION_COOLDOWN_MAX_SECONDS : f32 = 2.0;
 
 #[derive(Component)]
-pub struct BounceAnimation {
-	pub playing : bool,
-	pub initial_position : Vec3,
+pub struct Bounce {
+	pub bouncing : bool,
+	pub is_mid_bounce : bool,
 	pub initial_velocity_min : f32,
 	pub initial_velocity_max : f32,
-	pub current_velocity : f32
+	pub interval_min : f32,
+	pub interval_max : f32,
+	timer : Timer
 }
 
-impl BounceAnimation {
+impl Bounce {
 	pub fn new(
 		initial_velocity_min : f32,
-		initial_velocity_max : f32
-	) -> BounceAnimation {
+		initial_velocity_max : f32,
+		interval_min : f32,
+		interval_max : f32
+	) -> Bounce {
 
-		BounceAnimation {
-			playing : false,
-			initial_position : Vec3::new(0.0, 0.0, 0.0),
+		Bounce {
+			bouncing : false,
+			is_mid_bounce : false,
 			initial_velocity_min,
 			initial_velocity_max,
-			current_velocity : 0.0 
+			interval_min,
+			interval_max,
+			timer : Timer::from_seconds(
+				rand::random::<f32>() + 0.5,
+				TimerMode::Repeating
+			) 
 		}
 	}
 }
@@ -93,7 +107,7 @@ fn default_person() -> Text2d {
 }
 
 #[derive(Component)]
-#[require(Anchor(default_person_anchor), TextSprite, Text2d(default_person))]
+#[require(Anchor(default_person_anchor), Gravity, TextSprite, Text2d(default_person))]
 pub struct PersonSprite{
 	pub in_danger : bool,
 	pub animaton_interval_timer : Timer
@@ -104,7 +118,7 @@ impl Default for PersonSprite {
 		PersonSprite {
 			in_danger : false,
 			animaton_interval_timer: Timer::from_seconds(
-				rand::random::<f32>() + 0.5,
+				rand::random::<f32>(),
 				TimerMode::Repeating
 			) 
 		}
@@ -112,35 +126,76 @@ impl Default for PersonSprite {
 }
 
 impl PersonSprite {
+
+	pub fn bounce(
+		time: Res<Time>,
+		mut query: Query<(
+			&mut Bounce,
+			&mut Velocity,
+			&Gravity
+		)>
+	) {
+		let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
+		for (mut bounce, mut velocity, gravity) in query.iter_mut() {
+
+			if !bounce.is_mid_bounce {
+				bounce.timer.tick(time.delta());
+			}
+
+			if bounce.is_mid_bounce {
+
+				let interval_max = bounce.interval_max;
+				let interval_min  = bounce.interval_max;
+
+				if !gravity.is_falling {
+					bounce.is_mid_bounce = false;
+					bounce.timer.set_duration(
+						Duration::from_secs_f32(rand::random::<f32>()
+						* (interval_max - interval_min) 
+						+ interval_min)
+					);
+					bounce.timer.reset();
+				}
+			} else {
+				if bounce.timer.finished() && !gravity.is_falling && bounce.bouncing {
+					bounce.is_mid_bounce = true;
+					velocity.0 = Vec3::new(0.0, rng.gen_range(bounce.initial_velocity_min..bounce.initial_velocity_max), 0.0);
+				}
+			}
+		}
+	}
+
 	pub fn animate(
 		time: Res<Time>,
-		mut query: Query<(Entity,  &mut Text2d, &TransientAudioPallet, &mut TextColor, &mut Transform, &mut PersonSprite, &mut BounceAnimation)>,
+		mut query: Query<(
+			Entity,  
+			&mut Text2d, 
+			&TransientAudioPallet, 
+			&mut TextColor,
+			&mut Transform, 
+			&mut PersonSprite, 
+			&mut Bounce,
+			&mut Velocity,
+			&Gravity
+		)>,
 		mut commands : Commands,
 		mut audio_query: Query<&mut TransientAudio>,
 	) {
-
-		let duration_seconds = time.delta().as_secs_f32();
 		let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
 
-		for (entity, mut text, pallet, mut color,  mut transform, mut person, mut animation) in query.iter_mut() {
+		for (entity, mut text, pallet, mut color,  mut transform, mut person, mut animation, mut velocity, gravity) in query.iter_mut() {
 		
-			if person.in_danger && !animation.playing {
+			if person.in_danger && !animation.is_mid_bounce {
 				person.animaton_interval_timer.tick(time.delta());
 			}
 
-			if animation.playing {
+			if animation.is_mid_bounce {
 				text.0 = String::from(PERSON_IN_DANGER);
 				transform.scale = Vec3::ONE;
 				color.0 = DANGER_COLOR;
 
-				if transform.translation.y >= animation.initial_position.y {
-					transform.translation.y += animation.current_velocity*duration_seconds;
-					animation.current_velocity += ACCELERATION_DUE_TO_GRAVITY*duration_seconds;
-				} else {
-					animation.playing = false;
-					transform.translation.y = animation.initial_position.y;
-					animation.current_velocity = 0.0;
-
+				if !gravity.is_falling {
+					animation.is_mid_bounce = false;
 					person.animaton_interval_timer.set_duration(
 						Duration::from_secs_f32(rand::random::<f32>()
 						* (ANIMATION_COOLDOWN_MAX_SECONDS - ANIMATION_COOLDOWN_MIN_SECONDS) 
@@ -148,14 +203,13 @@ impl PersonSprite {
 					);
 					person.animaton_interval_timer.reset();
 				}
-			
 			} else {
 				text.0 = String::from(PERSON);
 				color.0 = PRIMARY_COLOR;
 				transform.scale = Vec3::ONE;
-				if person.animaton_interval_timer.finished() && (transform.translation.y == animation.initial_position.y) && person.in_danger {
-					animation.playing = true;
-					animation.current_velocity = rng.gen_range(animation.initial_velocity_min..animation.initial_velocity_max);
+				if person.animaton_interval_timer.finished() && !gravity.is_falling && person.in_danger {
+					animation.is_mid_bounce = true;
+					velocity.0 = Vec3::new(0.0, rng.gen_range(animation.initial_velocity_min..animation.initial_velocity_max), 0.0);
 					TransientAudioPallet::play_transient_audio(
                         entity,
                         &mut commands,
@@ -204,14 +258,14 @@ impl Default for Emoticon {
 impl Emoticon {
 	pub fn animate(
 		time: Res<Time>,
-		person_query: Query<&mut BounceAnimation>,
+		person_query: Query<&mut Bounce>,
 		mut emoticon_query: Query<(&Parent, &mut Emoticon, &mut Transform, &mut Text2d, &mut TextColor)>,
 	) {
 
 		let duration_seconds = time.delta().as_secs_f32();
 		for (parent, mut sprite, mut transform, mut text, mut color) in emoticon_query.iter_mut() {
 			if let Ok(animation) = person_query.get(parent.get()) {
-				if animation.playing {
+				if animation.is_mid_bounce {
 					sprite.current_size += duration_seconds*2.0;
 					transform.translation.y += duration_seconds*15.0;
 					transform.scale = Vec3::new(
